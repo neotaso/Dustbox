43435413ad65acee9245f04ec5883fd85205cc0d
neotaso <neotaso@mma.club.uec.ac.jp>
2014-05-01 11:52:31 +0900

    add ring_yade.rb

---
+++ ring_yade.rb
@@ -0,0 +1,12 @@
+#!/usr/local/bin/ruby
+# encoding:utf-8
+require 'rubygems'
+require 'mechanize'
+
+agent = Mechanize.new
+url = 'http://yadex-mail.herokuapp.com/'
+agent.get(url)
+agent.page.form_with(:name => nil) do |form|
+  form.click_button
+end
+puts agent.page.search('div.alert').to_s.gsub(/<("[^"]*"|'[^']*'|[^'">])*>/, "").gsub(/\s*\n*/, "")
